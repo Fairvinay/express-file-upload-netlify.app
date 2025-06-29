e8b9a188440738330daf9dd6472f24e94a9b6f0d
Vinayak Anvekar <fairvinay@gmail.com>
2025-06-29 19:10:31 +0530

    clear and close task that take binary stream from frabjous-truffle-675e06.netlify.app and downloads in word

--- server.js
+++ server.js
@@ -1,21 +1,117 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
-
+const fs = require('fs');
+const path = require('path');
+const exportPDFUtil = require('./src/exportpdf/export-pdf-to-docx-with-ocr-options')
 global.__basedir = __dirname;
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "https://frabjous-truffle-675e06.netlify.app"
+  //origin: "http://192.168.1.7:4233"
+ // origin: "http://localhost:4233"
 };
 
 app.use(cors(corsOptions));
-
-const initRoutes = require("./src/routes");
-
 app.use(express.urlencoded({ extended: true }));
-initRoutes(app);
+app.use(express.json());
+const initRoutes = require("./src/routes");
+// Middleware to parse raw binary data
+// Limit the payload size as needed (e.g., 10MB)
+app.use(express.raw({ type: 'application/octet-stream', limit: '10mb' })); 
+
+app.post('/upload-binary',async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send('No data provided in the request body.');
+  }
+
+  // Define the path where the file will be saved
+  const fileName = 'uploaded_binary_file.pdf'; // You might generate a unique name
+  const filePath = path.join(__dirname, 'uploads', fileName);
+
+  // Ensure the 'uploads' directory exists
+  if (!fs.existsSync(path.join(__dirname, 'uploads'))) {
+    fs.mkdirSync(path.join(__dirname, 'uploads'));
+  }
+
+  // Write the raw buffer to a file
+  fs.writeFile(filePath, req.body, async(err) => {
+    if (err) {
+      console.error('Error saving file:', err);
+      return res.status(500).send('Error saving file.');
+    }
+    // cinvert to DOCX 
+   let outfile = await exportPDFUtil( fileName );
+
+
+
+    res.status(200).send(`File "${outfile}" uploaded successfully.`);
+  });
+});
+
+app.get('/dowload-binary/:name', (req, res) => {
+  let fileName = req.params.name;
+   console.log('Requested file:', fileName);
+  let bodyFile = '';
+  if ( fileName !==undefined){
+      console.log("dowload-binary  request params  found ");
+
+  }
+  else {
+      bodyFile = req.body.name;
+    fileName = bodyFile;
+  }
+   console.log("dowload-binary file name "+fileName)
+  const directoryPath = __basedir + "/output/ExportPDFToDOCXWithOCROptions/";
+  const filePath = path.join(directoryPath, fileName);
+  res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
+  res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+
+    res.sendFile(filePath, (err) => {
+      if (err) {
+        res.status(500).send({
+          message: "Could not send the DOCX file. " + err,
+        });
+      }
+    });
+
+  /*
+  res.download(directoryPath + fileName, fileName, (err) => {
+    if (err) {
+      res.status(500).send({
+        message: "Could not download the file. " + err,
+      });
+    }
+  }); */
+
+  // Define the path where the file will be saved
+ /* const fileName = 'uploaded_binary_file.pdf'; // You might generate a unique name
+  const filePath = path.join(__dirname, 'uploads', fileName);
+
+  // Ensure the 'uploads' directory exists
+  if (!fs.existsSync(path.join(__dirname, 'uploads'))) {
+    fs.mkdirSync(path.join(__dirname, 'uploads'));
+  }
+
+  // Write the raw buffer to a file
+  fs.writeFile(filePath, req.body, (err) => {
+    if (err) {
+      console.error('Error saving file:', err);
+      return res.status(500).send('Error saving file.');
+    }
+    // cinvert to DOCX 
+    exportPDFUtil( fileName );
+     res.status(200).send(`File "${fileName}" uploaded successfully.`);
+  });*/
+});
+
+
+ const hostname = '0.0.0.0'; // Or your specific IP address, e.g., '192.168.1.100'
+
+//app.use(express.urlencoded({ extended: true }));
+//initRoutes(app);
 
 let port = 8080;
-app.listen(port, () => {
+app.listen(port,hostname, () => {
   console.log(`Running at localhost:${port}`);
 });
